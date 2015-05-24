3277621e1302d803babe3eef76da74a5a1ce190c
xeriscape <fb@consideredharmful.info>
2015-05-24 23:16:46 +0200

    Try-catch structure added

--- automate_search.py
+++ automate_search.py
@@ -4,15 +4,23 @@ import glob, os
 #------------------------------------------------------------------------------
 def main():
 	for search_file in glob.glob("*.srch"):
-		x = open(search_file, "rb").readlines()
-		query = x[0].replace("\n","").replace("\r","")
-		since = x[1].replace("\n","").replace("\r","")
-		until = x[2].replace("\n","").replace("\r","")
-		crsor = x[3].replace("\n","").replace("\r","")
-		is_realtime = True
+		try:
+			x = open(search_file, "rb").readlines()
+			query = x[0].replace("\n","").replace("\r","")
+			since = x[1].replace("\n","").replace("\r","")
+			until = x[2].replace("\n","").replace("\r","")
+			crsor = x[3].replace("\n","").replace("\r","")
+			is_realtime = True
 		
-		print "\n--------- Executing search ",x," ---------\n"
-		websearch_scrape.execute_search(query, since, until, crsor, is_realtime)
+			print "\n--------- Executing search ",x," ---------\n"
+			websearch_scrape.execute_search(query, since, until, crsor, is_realtime)
+			
+		except (KeyboardInterrupt, SystemExit):
+			print "Process aborted."
+			raise
+			
+		except:
+			print "Error at ", x
 
 #------------------------------------------------------------------------------
 if __name__ == "__main__":
