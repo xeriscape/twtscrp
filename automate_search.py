3f633b7689218e4d1775d7b15be8f1a77e72a5d7
xeriscape <fb@consideredharmful.info>
2015-05-24 15:41:37 +0200

    Added initial "automated search" functionality
    You can now specify search terms ahead of time, keep them in .SRCH
    files, and then bulk execute searches with automate_search.py. Updated
    Readme to reflect this.

---
+++ automate_search.py
@@ -0,0 +1,20 @@
+import websearch_scrape
+from websearch_scrape import get_search_chunk, extract_tweets, execute_search
+import glob, os
+#------------------------------------------------------------------------------
+def main():
+	for search_file in glob.glob("*.srch"):
+		x = open(search_file, "rb").readlines()
+		query = x[0].replace("\n","").replace("\r","")
+		since = x[1].replace("\n","").replace("\r","")
+		until = x[2].replace("\n","").replace("\r","")
+		crsor = x[3].replace("\n","").replace("\r","")
+		is_realtime = True
+		
+		print "\n--------- Executing search ",x," ---------\n"
+		websearch_scrape.execute_search(query, since, until, crsor, is_realtime)
+
+#------------------------------------------------------------------------------
+if __name__ == "__main__":
+	''' The usual boilerplate... '''
+	main()
